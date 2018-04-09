11b421e6601c6f61398bf6f9bdd667e9757efceb
Klaus Zerbe <klaus@zerbe-net.org>
2018-04-09 08:53:29 +0200

    documented demo code

--- src/components/system-global-settings.component.ts
+++ src/components/system-global-settings.component.ts
@@ -1,6 +1,9 @@
-import {Component, Input} from '@angular/core';
+import {Component} from '@angular/core';
 import {GlobalSettings, SystemGlobalService} from "../app/system-global-service";
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+
+// regex for checking IP validity
+const IP_VALID = /^([1-9]?\d|1\d\d|2[0-4]\d|25[0-5])\.([1-9]?\d|1\d\d|2[0-4]\d|25[0-5])\.([1-9]?\d|1\d\d|2[0-4]\d|25[0-5])\.([1-9]?\d|1\d\d|2[0-4]\d|25[0-5])$/;
 
 @Component({
      selector: 'system-global-settings',
@@ -10,45 +13,40 @@ import {FormBuilder, FormGroup, Validators} from "@angular/forms";
  })
 export class SystemGlobalSettingsComponent
 {
-    private static REGEX = /^([1-9]?\d|1\d\d|2[0-4]\d|25[0-5])\.([1-9]?\d|1\d\d|2[0-4]\d|25[0-5])\.([1-9]?\d|1\d\d|2[0-4]\d|25[0-5])\.([1-9]?\d|1\d\d|2[0-4]\d|25[0-5])$/;
-    @Input() initialSetup = false;
+    changedSettings: GlobalSettings; // monitor form changes
+    updatedSettings: GlobalSettings; // return updated form data
+    settingsGroup: FormGroup;        // form control hierarchy
 
-    changedSettings: GlobalSettings;
-    updatedSettings: GlobalSettings;
-    settingsGroup: FormGroup;
-    otherServerIP: string;
-
+    // getters for all form controls
     get hostname() { return this.settingsGroup.controls['hostname']; }
     get gateway() { return this.settingsGroup.controls['gateway']; }
     get nameServer() { return this.settingsGroup.controls['nameServer']; }
     get ntpServer() { return this.settingsGroup.controls['ntpServer']; }
 
-    nameserverValidation = [Validators.required, Validators.pattern(SystemGlobalSettingsComponent.REGEX)];
+    private ipValidation = Validators.pattern(IP_VALID);
 
     constructor(private systemGlobalService: SystemGlobalService, fb: FormBuilder)
     {
+      // build form control hierarchy
       this.settingsGroup = fb.group({
         hostname: fb.control('', [Validators.required, Validators.minLength(1)]),
-        gateway: fb.control('',
-          [Validators.required, Validators.pattern(SystemGlobalSettingsComponent.REGEX)]),
-        nameServer: fb.control('',
-          [Validators.required, Validators.pattern(SystemGlobalSettingsComponent.REGEX)]),
+        gateway: fb.control('', [Validators.required, this.ipValidation]),
+        nameServer: fb.control('',[Validators.required, this.ipValidation]),
         ntpServer: fb.control('', [Validators.required, Validators.minLength(1)]),
       });
 
+      // observe data to edit
       this.systemGlobalService.globalSettings$.subscribe((settings => {
         this.settingsGroup.setValue(settings);
       }));
 
+      // observe updated form data
       this.settingsGroup.valueChanges.subscribe(settings => this.changedSettings = settings);
     }
 
+    // show updated data
     applyClicked(event): void
     {
       this.updatedSettings = this.changedSettings;
-    }
-
-    otherServerChanged(data: string) {
-      this.otherServerIP = data;
     }
 }
--- src/components/validated-input.component.ts
+++ src/components/validated-input.component.ts
@@ -7,7 +7,7 @@ import {AbstractControl, ControlValueAccessor, NG_VALUE_ACCESSOR} from "@angular
     <div class="form-group has-feedback" [ngClass]="{'has-error':!control.valid}">
       <label for="inputField" class="col-sm-2 control-label">{{label}}</label>
       <div class="col-sm-3">
-        <input type="text" class="form-control" [value]="inputValue" (keyup)="onInputChange($event.target.value)" id="inputField" placeholder="Enter Name Server">
+        <input type="text" class="form-control" [value]="inputValue" (keyup)="onInputChange($event.target.value)" id="inputField" placeholder="Enter Server IP">
         <span *ngIf="!control.valid" class="glyphicon glyphicon-remove form-control-feedback"></span>
         <h6 *ngIf="!control.valid" class="form-text text-danger">{{errorMessage}}</h6>
       </div>
@@ -18,24 +18,28 @@ import {AbstractControl, ControlValueAccessor, NG_VALUE_ACCESSOR} from "@angular
       </div>
     </div>
   `,
-  styleUrls: ['./system-global-settings.component.css'],
 
   providers: [
+    // allow NgControl injection to let FormControl manage this component
     { provide: NG_VALUE_ACCESSOR,
       useExisting: forwardRef(() => ValidatedInputComponent),
       multi: true}
     ]
 })
+// create a custom FormControl
 export class ValidatedInputComponent implements ControlValueAccessor {
   @Input() label: string;
   @Input() errorMessage;
-  @Input() control: AbstractControl;
-  inputValue: string;
+  @Input() control: AbstractControl; // the owning form control
 
+  inputValue: string; // input field data
+
+  // fill the input with model data
   writeValue(value: string) {
     this.inputValue = value;
   }
 
+  // return changed data
   onInputChange(value: string) {
     this.onChange(value)
   }
@@ -43,6 +47,7 @@ export class ValidatedInputComponent implements ControlValueAccessor {
   onChange = (fn) => { this.onChange = fn};
   onTouched = () => {};
 
+  // register for touch/change handling
   registerOnChange(fn): void { this.onChange = fn; }
   registerOnTouched(fn): void { this.onTouched = fn; }
 }
